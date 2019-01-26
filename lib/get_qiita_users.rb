37ac06857f0f26c0246bb0d9d9ad4604505c0a4a
Masayuki Goto <originals0711@gmail.com>
2019-01-26 23:14:52 +0900

    Update csv generate process

--- lib/get_qiita_users.rb
+++ lib/get_qiita_users.rb
@@ -1,6 +1,23 @@
-require 'pry'
+require 'csv'
 require 'dotenv/load'
 require 'qiita'
 
-binding.pry
-puts 1
+PER_PAGE = 100.freeze
+
+client = Qiita::Client.new(access_token: ENV['QIITA_API_TOKEN'])
+first_users = client.list_users(per_page: PER_PAGE)
+csv_header = first_users.body.first.keys
+
+CSV.open('qiita_users.csv', 'w') do |csv|
+  csv << csv_header
+  first_users.body.each do |user|
+    csv << user.values
+  end
+  # TODO: DRY にできないか？
+  (2..50).each do |n|
+    items = client.list_users(page: n, per_page: PER_PAGE)
+    items.body.each do |item|
+      csv << item.values
+    end
+  end
+end
